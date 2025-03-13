d05c1b50f014a952efeca91938ceff753e44382e
Carlos Miguel Viales Solorzano <carviaso@users.noreply.github.com>
2025-03-13 13:25:03 -0600

    Update Calcular el Índice de Masa Corporal (IMC) de un paciente.py

--- Calcular el Índice de Masa Corporal (IMC) de un paciente.py
+++ Calcular el Índice de Masa Corporal (IMC) de un paciente.py
@@ -1,9 +1,12 @@
 # Solicita la formacion para calcular el IMC
+## Entrada de datos
 peso = float(input("Ingrese el peso del paciente (kg): "))
 altura = float(input("Ingrese la altura del paciente (m): "))
 
+# Cálculos (peso / altura^2)
 imc = peso / (altura ** 2)  # Fórmula del IMC
 
+# Condiciones (if, elif, else)
 if imc < 18.5:
     categoria = "Bajo peso"
 elif imc < 25:
@@ -13,4 +16,5 @@ elif imc < 30:
 else:
     categoria = "Obesidad"
 
+# Formato de salida ({imc:.2f} redondea a 2 decimales)
 print(f"El IMC del paciente es {imc:.2f}, categoría: {categoria}")
